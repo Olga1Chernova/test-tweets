e11db2e5b32e0bb144a681135d552ef3dbee0eab
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-06-05 17:32:16 +0300

    updated TweetList component

--- src/components/TweetsList/TweetsList.jsx
+++ src/components/TweetsList/TweetsList.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { getAllUsers } from 'shared/services/tweets-api';
 import { SingleTweet } from 'components/SingleTweet/SingleTweet';
 
-import css from './TweetList.module.css'
+import css from './TweetList.module.css';
 
 export const TweetsList = () => {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(3); 
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -20,14 +22,29 @@ export const TweetsList = () => {
     fetchUsers();
   }, []);
 
-    return (
-      <>
-        <ul className={css.mainContainer}>
-          {users.map(user => (
-            <SingleTweet key={user.id} user={user} />
-          ))}
-        </ul>
-        <button>LOAD MORE</button>
-      </>
-    );
+  const totalUsers = users.length;
+  const lastPage = Math.ceil(totalUsers / usersPerPage);
+
+  const handleLoadMore = () => {
+    const nextPage = currentPage + 1;
+    setCurrentPage(nextPage);
+  };
+
+  const showLoadMoreButton = currentPage < lastPage;
+
+  const visibleUsers = users.slice(0, currentPage * usersPerPage);
+
+  return (
+    <>
+      <ul className={css.mainContainer}>
+        {visibleUsers.map(user => (
+          <SingleTweet key={user.id} user={user} />
+        ))}
+      </ul>
+      {showLoadMoreButton && (
+        <button onClick={handleLoadMore}>LOAD MORE</button>
+      )}
+    </>
+  );
 };
+
